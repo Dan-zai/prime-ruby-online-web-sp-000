d9e5cde5a1a1e5a32fc3a52389e25acb669d800f
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-06 04:00:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,6 @@
 def prime?(int)
     return false if int < 2
-    (2..int - 1).each do |x|
-        if (int % x) == 0
-            return false
-        end
+    (2..int - 1).each { |x| if (int % x) == 0 return false}
     end
     true
 end
